63f8d05f65028634c7ca0a70d3dccc03775a93a4
Brian Bawden <brianbawden001@gmail.com>
2022-06-04 22:24:37 -0400

    Start git tracking

---
+++ python_sql.py
@@ -0,0 +1,32 @@
+# Youtube websie used: https://www.youtube.com/watch?v=xY54Emo8rQM
+
+import pandas as pd
+import sqlite3 as db
+
+conn = db.connect('sqlite.db')
+cur = conn.cursor()
+
+
+def create_customer_table():
+    cur.execute(f"CREATE TABLE IF NOT EXISTS customers (customer_id INTEGER PRIMARY KEY, first_name NVARCHAR(50), last_name NVARCHAR(50), p_number NVARCHAR(12), address NVARCHAR(50))")
+
+def add_customer(cu_id, first_name, last_name, phone_num, address):
+    cur.execute(f"INSERT INTO customers(rowid, first_name, last_name, p_number, address) VALUES('{cu_id}', '{first_name}', '{last_name}', '{phone_num}', '{address}')")
+
+def update_last_name(cu_id, new_name):
+    cur.execute(f"UPDATE customers SET last_name = '{new_name}' WHERE customer_id = {cu_id}")
+
+def show_table():
+    cur.execute("SELECT * FROM customers")
+    for row in cur.fetchall():
+        print(row)
+
+def commit():
+    print("Commiot")
+    conn.commit()
+
+
+show_table()
+# update_last_name(2, 'lenke')
+# show_table()
+# commit()
